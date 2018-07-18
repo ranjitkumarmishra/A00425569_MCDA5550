880acb5611c658bb405c062ca706e1855dc9796a
Ranjit Mishra <ranjitkumarmishra@gmail.com>
2018-07-18 17:39:41 -0300

    Added InClassDatabaseHelper class

---
+++ assignment1/app/src/main/java/com/example/ranjit/assignment1/InClassDatabaseHelper.java
@@ -0,0 +1,38 @@
+package com.example.ranjit.assignment1;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.Date;
+
+public class InClassDatabaseHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "inclass"; // name of the DB
+    private static final int DB_VERSION = 1; // version of the DB
+    public static final String TABLE_NAME = "PERSON"; // name of the Table
+    public InClassDatabaseHelper(Context context){
+        super(context,DB_NAME,null, DB_VERSION); // null is for cursors
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        System.out.println("onCreate InClassDatabaseHelper");
+        db.execSQL("CREATE TABLE "+TABLE_NAME+" ("
+                + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "NAME TEXT, "
+                + "PASSWORD TEXT, " // Never store passwords in clear text in real apps
+                + "HEALTH_CARD_NUMB TEXT, "
+                + "DATE INTEGER);");
+        Date today = new Date(); // we want to start with some initial data
+        ContentValues personValues = new ContentValues();
+        personValues.put("NAME", "Ranjit Mishra");
+        personValues.put("PASSWORD", "SuperSecret");
+        personValues.put("HEALTH_CARD_NUMB", "123456789101");
+        personValues.put("DATE", today.getTime());
+        db.insert(TABLE_NAME,null, personValues);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
--- assignment1/app/src/main/java/com/example/ranjit/assignment1/MainActivity.java
+++ assignment1/app/src/main/java/com/example/ranjit/assignment1/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.ranjit.assignment1;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        InClassDatabaseHelper helper = new InClassDatabaseHelper(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        // run a query
+        Cursor cursor = db.query(InClassDatabaseHelper.TABLE_NAME,new String[]
+                        {"NAME","PASSWORD","DATE"},
+                null,null,null,null,null); //
+        if (cursor.moveToFirst()){
+            String name = cursor.getString(0);
+            EditText results = (EditText) findViewById(R.id.personName);
+            results.setText(name);
+        }
+        cursor.close(); // cleanup
+        db.close(); // cleanup
     }
 
     public void onClickEnter(View view) {
